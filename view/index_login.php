c653c39986d851dcce2f8381289830ad1216594c
Spring Justin <bsprij@bbc-be.local>
2017-09-18 14:17:48 +0200

    Login Done

--- view/index_login.php
+++ view/index_login.php
@@ -1,18 +1,25 @@
 <div class="l-image"><img src="images/logoTMS.png"></div>
-<div class="l-title"><h2>Login</h2></div>
+<h2>Task erfassen</h2>
 <form class="form-horizontal" action="/user/doCreate" method="post">
 	<div class="component" data-html="true">
-<div class="l-LoginEngabe">
-    <div class="form-group">
-		  <label class="col-md-2 control-label" for="benutzername">Benutzername</label>
+
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="lastName">Benutzername</label>
 		  <div class="col-md-4">
-		  	<input id="l-Benutzername" name="benutzername" type="text" placeholder="Benutzername" class="form-control input-md">
+		  	<input id="taskTitel" name="taskTitel" type="text" placeholder="Benutzername" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
-		  <label class="col-md-2 control-label" for="password">Passwort</label>
+		  <label class="col-md-2 control-label" for="email">Passwort</label>
 		  <div class="col-md-4">
-		  	<input id="l-Passwort" name="passwort" type="password" placeholder="Passwort" class="form-control input-md">
+		  	<input rows="4" cols="50" id="beschreibung" name="beschreibung" type="password" placeholder="Passwort" class="form-control input-md">
+		  </div>
+		</div>
+
+		<div class="form-group">
+	      <label class="col-md-2 control-label" for="send">&nbsp;</label>
+		  <div class="col-md-4">
+		    <input id="send" name="send" type="submit" class="btn btn-primary">
 		  </div>
 		</div>
 	</div>
