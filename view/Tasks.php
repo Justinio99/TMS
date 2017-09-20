fa24de6b4e0ef9530fdfd28338c934dec416d6e2
Spring Justin <bsprij@bbc-be.local>
2017-09-20 09:05:18 +0200

    Added Picture to Home Page

--- view/Tasks.php
+++ view/Tasks.php
@@ -5,47 +5,55 @@
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
   <section>
     <label>Beschreibung:</label><p class="p">reaks to two columns without needing extra elements, classes, or JS</p>
     <label>Datum:</label><p  class="p">20.9</p>
     <label>Benuter:</label><p class="p">Justin</p>
+    <i class="fa fa-trash-o trash" aria-hidden="true"></i>
   </section>
 </article>
 <br>
-<a class="h-btn1" href="/deleteTask">Task löschen</a>
